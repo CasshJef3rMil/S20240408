ba9c7e4eb9e952ff1f2e8c67026f01d773ebf4ba
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-08 08:33:30 -0600

    Creacion APPddbContext y entidad SPersona

---
+++ S20240408.EntidadesDeNegocio/SPersona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S20240408.EntidadesDeNegocio
+{
+    public class SPersona
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Apellido { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public decimal Sueldo { get; set; }
+
+        public byte Estatus { get; set; }
+
+    }
+}
