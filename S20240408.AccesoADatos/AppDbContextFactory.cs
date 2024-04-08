4b97cfe3dbf19f8150dfb898b6db3f0181c0b007
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-08 08:57:20 -0600

    cree y termine de programar en la clase AppDbContextFactory

---
+++ S20240408.AccesoADatos/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S20240408.AccesoADatos
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            const string conn = "Data Source=.;Initial Catalog=S20240408DB;Integrated Security=True;Encrypt=False";
+            optionsBuilder.UseSqlServer(conn);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
