ec1bfe7a2e24dce356b84a2a864593eaf9e2fcb6
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-08 09:19:30 -0600

    agregar y progamar clase DependecyContainer

---
+++ S20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddBLDependecies(configuration);
+            services.AddScoped<SpersonaBL>();
+            return services;
+        }
+    }
+}
