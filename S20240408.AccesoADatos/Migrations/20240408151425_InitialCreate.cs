6ef3a61861b46d8258a3dcc3b8fb65fbe39eac7a
Jeferson Josue Muñoz Lopez <Jefer10Mil@DESKTOP-EC207QH>
2024-04-08 09:17:13 -0600

    crear y aplicar Migracion

---
+++ S20240408.AccesoADatos/Migrations/20240408151425_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S20240408.AccesoADatos.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spersonas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sueldo = table.Column<decimal>(type: "decimal(18,2)", maxLength: 60, nullable: false),
+                    Estatus = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spersonas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spersonas");
+        }
+    }
+}
